c6db1f8eb1923d727a066afa9f852074885a0253
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-09-10 12:39:38 +0200

    load and print board

--- project1/module1.py
+++ project1/module1.py
@@ -12,39 +12,27 @@ def loadBoard(filename): #Function to read from file
     for line in state:
         print "car:", state.index(line)
         print line[1], line[2]
-
         board[line[1]][line[2]] = state.index(line)
 
-    #tall = 0 #A number used to increment
-
-    """
-    for line in f: #Goes through the txt file one line at a time
-        board.append([]) #Elements in each line so it can be a 2-dimensional list
-        for x in range(len(line)):
-            if line[x] == '#': #Initilizes the walls whit a infinte weight
-                board[tall].append(Node(tall, x, line[x], float("inf")))
-            elif line[x] == 'w':
-                board[tall].append(Node(tall, x, line[x], 100))
-            elif line[x] == 'm':
-                board[tall].append(Node(tall, x, line[x], 50))
-            elif line[x] == 'f':
-                board[tall].append(Node(tall, x, line[x], 10))
-            elif line[x] == 'g':
-                board[tall].append(Node(tall, x, line[x], 5))
-            elif line[x] == 'r':
-                board[tall].append(Node(tall, x, line[x], 1))
-            else: #Initializes the rest of the nodes with weight 1
-                board[tall].append(Node(tall, x, line[x], 1))
-
-        del board[tall][-1] #Deletes the '\n'
-        tall += 1
-    """
+        for i in range(line[3]):
+            print i
+            if line[0] == 0:
+                board[line[1] + i][line[2]] = state.index(line)
+            else:
+                board[line[1]][line[2] + i] = state.index(line)
     f.close()
     return board
+
+def printBoard(board):
+    for i in range(6):
+        x = ''
+        for j in range(6):
+            x += str(board[j][i])
+        print x
 
 def run():
     filename = "module1/Scenarios/easy-3.txt"
     board = loadBoard(filename)
-    print board
+    printBoard(board)
 
 run()
