32514e55f31a6b9ccf9a32c3aefc093bdbd6fd5a
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-09-10 14:02:15 +0200

    find legal states

--- project1/module1.py
+++ project1/module1.py
@@ -21,7 +21,7 @@ def loadBoard(filename): #Function to read from file
             else:
                 board[line[1]][line[2] + i] = state.index(line)
     f.close()
-    return board
+    return board, state
 
 def printBoard(board):
     for i in range(6):
@@ -30,9 +30,52 @@ def printBoard(board):
             x += str(board[j][i])
         print x
 
+def findLegalMoves(board, state):
+    validStates = []
+    for car in state:
+        print "car ", state.index(car)
+        if car[0] == 0:
+            if board[car[1] - 1][car[2]] == 'x' and car[1] != 0: # Check to see if the car can move to the left
+                print "valid"
+                tempState = state[:]
+                tempState[state.index(car)] = [car[0], car[1] - 1, car[2], car[3]]
+                print tempState
+                validStates.append(tempState)
+                #validMove.append([car[0], car[1] - 1, car[2], car[3]])
+
+            if car[1]+car[3] < 6:
+                if board[(car[1] + car[3])][car[2]] == 'x' : # Check to see if the car can move to the right
+                    print "valid"
+                    tempState = state[:]
+                    tempState[state.index(car)] = [car[0], car[1] + 1, car[2], car[3]]
+                    validStates.append(tempState)
+
+        else:
+            if board[car[1]][car[2] -1] == 'x' and car[2] != 0: # check to see if the car can move up
+                print "valid"
+                tempState = state[:]
+                tempState[state.index(car)] = [car[0], car[1] , car[2]-1, car[3]]
+                validStates.append(tempState)
+            if car[2] + car[3] < 6:
+                if board[car[1]][car[2] + car[3]] == 'x': # check to see if the car can move up
+                    print "valid"
+                    tempState = state[:]
+                    tempState[state.index(car)] = [car[0], car[1] , car[2]+1, car[3]]
+                    validStates.append(tempState)
+    '''
+    print "0", validStates[0]
+    print "1", validStates[1]
+    print "2", validStates[2]
+    
+    '''
+    for i in validStates:
+        print i
+
+
+
 def run():
     filename = "module1/Scenarios/easy-3.txt"
-    board = loadBoard(filename)
+    board, state = loadBoard(filename)
     printBoard(board)
-
+    findLegalMoves(board,state)
 run()
